b0592682b3ff4837128b3c1c80b25d7aaf89af8b
Opuhliy Vladyslav <opuhliy.vladyslav@gmail.com>
2016-05-27 14:10:06 +0300

    sort by popular (position) fix

--- app/code/local/Brander/Shop/Model/Catalog/Layer.php
+++ app/code/local/Brander/Shop/Model/Catalog/Layer.php
@@ -10,6 +10,7 @@ class Brander_Shop_Model_Catalog_Layer extends Mage_Catalog_Model_Layer
             $collection = $this->getCurrentCategory()->getProductCollection();
             $collection->joinField('inventory_in_stock', 'cataloginventory_stock_item', 'is_in_stock', 'product_id=entity_id','is_in_stock>=0', 'left')->setOrder('inventory_in_stock', 'desc');
             $this->prepareProductCollection($collection);
+            $collection->setOrder('entity_id', 'asc');
             $this->_productCollections[$this->getCurrentCategory()->getId()] = $collection;
         }
         return $collection;
