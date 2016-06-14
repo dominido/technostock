5d99e5b10b5dfbeda4343d70300793389dd9e320
Alexandr Belyaev <alexvegas.developer@gmail.com>
2016-06-14 14:05:47 +0300

    new import

--- shell/start_import.php
+++ shell/start_import.php
@@ -1,6 +1,4 @@
 <?php
-set_time_limit(0);
-ini_set('memory_limit','512M');
 require_once 'abstract.php';
 
 class Mage_Shell_Start_Import extends Mage_Shell_Abstract
@@ -9,7 +7,13 @@ class Mage_Shell_Start_Import extends Mage_Shell_Abstract
 
     public function run()
     {
-        $import = Mage::getModel('autoimport/import')->importStart();
+        //$import = Mage::getModel('autoimport/import')->cronImportStart();
+        
+       /* Mage::getModel('brandercml/importgpd')
+            ->setSourceFileName('tehnostok-import.xml')
+            ->setSourceFilePath('var/import/')
+            ->setConsoleLog(true)
+            ->run();*/
     }
 
 }
