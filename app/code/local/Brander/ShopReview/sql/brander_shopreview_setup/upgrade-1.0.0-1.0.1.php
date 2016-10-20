a69a1c45016ec026dcfdf6931cfa86214c20dbba
icefire <icefie@mail.ru>
2016-10-20 12:17:29 +0300

    fast fix

--- app/code/local/Brander/ShopReview/sql/brander_shopreview_setup/upgrade-1.0.0-1.0.1.php
+++ app/code/local/Brander/ShopReview/sql/brander_shopreview_setup/upgrade-1.0.0-1.0.1.php
@@ -6,7 +6,7 @@ $connection = $installer->getConnection();
 $installer->startSetup();
 
 $installer->getConnection()
-    ->changeColumn($installer->getTable('brander_shopreview/shopreview'), 'updated_at', 'updated_at', array(
+    ->changeColumn($installer->getTable('brander_shopreview/shopreview'), 'create_at', 'create_at', array(
         'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
         'nullable' => true,
     ));
