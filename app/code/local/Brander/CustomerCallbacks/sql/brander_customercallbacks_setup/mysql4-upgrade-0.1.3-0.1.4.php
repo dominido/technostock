0aa2711625d2f20f4e5e0c8d1b8e70e2e7f65133
icefire <icefie@mail.ru>
2016-11-24 17:37:14 +0200

    Long text

---
+++ app/code/local/Brander/CustomerCallbacks/sql/brander_customercallbacks_setup/mysql4-upgrade-0.1.3-0.1.4.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Brander CustomerCallbacks extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * @category       Brander
+ * @package        CustomerCallbacks
+ * @copyright      Copyright (c) 2015
+ * @license        http://opensource.org/licenses/mit-license.php MIT License
+ * @author         AlexVegas (Alexandr Belyaev) <alexandr.belyaev.only@gmail.com>
+ */
+$installer = $this;
+
+$installer->startSetup();
+$table = $this->getTable('brander_customercallbacks/callbacks');
+
+$installer->run("ALTER TABLE {$table} MODIFY current_url LONGTEXT NULL DEFAULT NULL");
+
+$installer->endSetup();
